f09d67daf596557f3f5e57df9ddf7400832e1796
Ohkubo KOHEI <o@kbn.one>
2025-10-10 14:27:04 +0000

    change config

--- server/config.ts
+++ server/config.ts
@@ -1,4 +1,4 @@
-const rpID = Deno.env.get("RP_ID") ?? "id.kbn.one";
-const rpName = Deno.env.get("RP_NAME") ?? "id.kbn.one";
+const rpID = Deno.env.get("RP_ID") ?? "kbn.one";
+const rpName = Deno.env.get("RP_NAME") ?? "kbn.one";
 
 export { rpID, rpName };
